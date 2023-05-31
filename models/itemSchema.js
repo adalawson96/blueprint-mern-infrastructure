fefe69715e1b27855a39320e136de3179010d45a
adalawson96 <adalawson96@gmail.com>
2023-05-31 20:20:59 +0200

    modified id on seed and update schema

--- models/itemSchema.js
+++ models/itemSchema.js
@@ -3,7 +3,7 @@ const Schema = require('mongoose').Schema;
 const itemSchema = new Schema({
   name: { type: String, required: true },
   image: { type: String, required: true},
-  category: { type: Schema.Types.ObjectId, ref: 'Category'},
+  category: [{ type: Schema.Types.ObjectId, ref: 'Category'}],
   price: { type: Number, required: true },
   detail: { type: String, required: true }
 }, {
